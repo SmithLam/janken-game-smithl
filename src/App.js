2bffd1688b863e808fffc73794ba5412bdc5fc55
SmithLam <lamphuoctuong@gmail.com>
2020-05-25 15:15:18 +0700

    Finalized

--- src/App.js
+++ src/App.js
@@ -4,38 +4,41 @@ import Box from './components/Box.js'
 import List from './components/List'
 
 //render via Function
-function App() {
-  return (
-    <body>
-      <h1>THE JANKEN GAME</h1>
-   <div className="render">
-     <Box name="Sword" status="won" src="https://i.ytimg.com/vi/kYfewzvRhn4/hqdefault.jpg"color="red"/>
-     <Box name="Spear" status="draw" src="https://cdn.gorillasurplus.com/images/products/large/COL95MEP.jpg" color="orange"/>
-     <Box name="Axe" status="lost" src="http://dreamicus.com/data/axe/axe-01.jpg" color="blue"/>
-      {/* <Box name="fourth"color="yellow" /> */}
+// function App() {
+//   return (
+//     <body>
+//       <h1>THE JANKEN GAME</h1>
+//    <div className="render">
+//      <Box name="Sword" status="won" src="http://darksouls3.wdfiles.com/local--files/image-set-equipment:long-sword/Long-Sword.png"color="red"/>
+//      <Box name="Spear" status="draw" src="http://darksouls3.wdfiles.com/local--files/image-set-equipment:pike/Pike.png" color="orange"/>
+//      <Box name="Axe" status="lost" src="http://darksouls3.wdfiles.com/local--files/image-set-equipment:hand-axe/Hand-Axe.png" color="blue"/>
+//       {/* <Box name="fourth"color="yellow" /> */}
 
-     {/* <ul>
-  <List/>
-  <List/>
-  <List/>
-     </ul> */}
-   </div>
-   </body>
-  );
-}
+//      {/* <ul>
+//   <List/>
+//   <List/>
+//   <List/>
+//      </ul> */}
+//    </div>
+//    </body>
+//   );
+// }
 //render via Function
 
-//render via Class
-// class App extends React.Component {
-//   render() {
-//     return <div>
-//             <Box name="Sword" status="won" src="https://i.ytimg.com/vi/kYfewzvRhn4/hqdefault.jpg"color="red"/>
-//             <Box name="Spear" status="draw" src="https://cdn.gorillasurplus.com/images/products/large/COL95MEP.jpg" color="orange"/>
-//             <Box name="Axe" status="lost" src="http://dreamicus.com/data/axe/axe-01.jpg" color="blue"/>
-//             </div>
-//   }
-// }
-//render via Class
+// render via Class
+class App extends React.Component {
+  render() {
+    return   <body>
+            <h1>THE JANKEN GAME</h1>
+            <div className="render">
+            <Box name="Sword" status="won" src="http://darksouls3.wdfiles.com/local--files/image-set-equipment:long-sword/Long-Sword.png"color="red"/>
+            <Box name="Spear" status="draw" src="http://darksouls3.wdfiles.com/local--files/image-set-equipment:pike/Pike.png" color="orange"/>
+            <Box name="Axe" status="lost" src="http://darksouls3.wdfiles.com/local--files/image-set-equipment:hand-axe/Hand-Axe.png" color="blue"/>
+            </div>
+            </body>
+  }
+}
+// render via Class
 
 
 export default App;
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,17 +1,50 @@
+
+//render component via function
 import React from 'react'
 
-
-
 export default function Box(props) {
+
+//     //in Function render, you can use "let" or "const"
+    const callme=(status)=>{
+        console.log(status)
+        alert("You have " + status +"!")
+    }
     return (
         <div className={`box ${props.color}`}  >
         {/* start wrap */}
         <p className={`para ${props.name}`}>My {props.name} is {props.color}</p>
         <p><img alt="weapon" src={props.src}/></p> 
         <p className={`para ${props.name}`}>and has {props.status}!</p>
-
+        <button onClick={()=>callme(props.status)}>Click me!</button>
         {/* end wrap */}
         </div>
     )
 }
+//render component via function
 
+
+//render component via class
+
+// import React, { Component } from 'react'
+// export default class Box extends Component {
+    
+//     //in Class function, you cannot use "let" or "const" only the ES6 way or normal function way
+//     callme = () =>{
+    // alert("You lose")
+//     }
+
+//     render() {
+//         return (
+//         <div className={`box ${this.props.color}`}  >
+//         {/* start wrap */}
+//         <p className={`para ${this.props.name}`}>My {this.props.name} is {this.props.color}</p>
+//         <p><img alt="weapon" src={this.props.src}/></p> 
+//         <p className={`para ${this.props.name}`}>and has {this.props.status}!</p>
+//         <button onClick={()=>this.callme()}>Click me!</button>
+//         {/* end wrap */}
+//         </div>
+//         )
+//     }
+// }
+
+//render component via class
