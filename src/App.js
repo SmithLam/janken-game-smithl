38996ca02bbad1c934a63b8e3f7f6e5f046d43dd
SmithLam <lamphuoctuong@gmail.com>
2020-05-26 16:17:54 +0700

    Commit with most issues

--- src/App.js
+++ src/App.js
@@ -1,22 +1,132 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Box from './components/Box.js'
 
 
+const CHOICES = {
+  scissors: {
+    name: "scissors",
+    url: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png"
+  },
+  paper: {
+    name: "paper",
+    url: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png"
+  },
+  rock: {
+    name: "rock",
+    url:
+      "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png"
+  }
+};
+
 // render via Function
 function App() {
+const saveUsername = () =>{
+  const newUsername = document.getElementById("input-field").value
+  localStorage.setItem('username', newUsername);
+  setUsername(newUsername);
+}
+
+const saveResult = () =>{
+  localStorage.setItem("GameHistory", JSON.stringify(gameHistory));
+}
+
+if (!localStorage.getItem('GameHistory')){
+  localStorage.setItem("GameHistory", JSON.stringify([]))
+}
+
+  let [userC, setUserC] = useState({}) //state
+  let [computerC, setComputerC] = useState({}) //state
+  const [username, setUsername] = useState(localStorage.getItem('username'))
+  const [previousWinner, setPreviousWinner] = useState("")//state
+  const [gameHistory, setGameHistory] = useState(JSON.parse(localStorage.getItem('GameHistory')));
+  const [started, setStarted] = useState(false);
+  // let [result, setResult] = useState(null) //state
+
+
+
+  const onplay = (userChoice) =>{
+    if (!username){
+      alert("You must enter a username")
+      return;
+    }
+    console.log("clicked", userChoice)
+    //define user's choice
+    setUserC(CHOICES[userChoice])
+    console.log("user chooses this one", CHOICES[userChoice])
+    //computer chooses logic should be here
+    //computer chooes random item
+    let computerArray = Object.keys(CHOICES) //get the key from the object Choices
+    let randomNum = Math.floor(Math.random()*(computerArray.length))
+    let computerName = computerArray[randomNum]
+
+    setComputerC (CHOICES[computerName])
+    console.log("what is computer choice", CHOICES[computerName])
+
+    let result =""
+
+    if (userChoice === "rock") {
+      result = CHOICES[computerName].name === "scissors" ? "Victory!" : "Defeat!";
+    }
+    if (userChoice === "paper") {
+      result = CHOICES[computerName].name === "rock" ? "Victory!" : "Defeat!";
+    }
+    if (userChoice === "scissors") {
+      result = CHOICES[computerName].name === "paper" ? "Victory!" : "Defeat!";
+    }
+    if (userChoice === CHOICES[computerName].name) result = "Tied!";
+    
+    console.log(result)
+
+    gameHistory.push(username+": "+result);
+    saveResult()
+    setGameHistory(gameHistory)
+
+    if (result === "Victory!"){
+      setPreviousWinner("You")
+    }
+    if (result === "Defeat!"){
+      setPreviousWinner("Computer")
+    }
+    if (result === "Tied!"){
+      setPreviousWinner("Tied")
+    }
+  }
+
   return (
     <body>
       <h1>THE JANKEN GAME</h1>
    <div className="render">
-     <Box name="Sword" status="won" src="http://darksouls3.wdfiles.com/local--files/image-set-equipment:long-sword/Long-Sword.png"color="red"/>
-     <Box name="Spear" status="draw" src="http://darksouls3.wdfiles.com/local--files/image-set-equipment:pike/Pike.png" color="orange"/>
-     <Box name="Axe" status="lost" src="http://darksouls3.wdfiles.com/local--files/image-set-equipment:hand-axe/Hand-Axe.png" color="blue"/>
+     <Box title ="You" choice={userC} player={previousWinner}  />
+
+     <div>
+       <button disabled={!started} onClick ={()=>onplay('rock')}>Rock</button>
+       <button disabled={!started} onClick ={()=>onplay('paper')}>Paper</button>
+       <button disabled={!started} onClick ={()=>onplay('scissors')}>Scissor</button>
+       <div>
+       {/* <h1>{result}</h1> */}
+       </div>
+       <div>
+       <button onClick={() => setStarted(!started)}>Start</button>
+       <input id="input-field" placeholder="username"></input>
+       <button onClick ={() => saveUsername()}>Submit</button>
+        <center><h3>History {username}</h3> </center>
+        <ul className="history-log">
+      {gameHistory.map(result => {
+      return <li>{result}</li>;
+      })}
+
+        </ul>
+       </div>
+      </div>
+
+     <Box title ="Computer" choice={computerC}  player={previousWinner}/>
   
    </div>
    </body>
-  );
+  )
 }
+
 // render via Function
 
 
--- src/components/Box.js
+++ src/components/Box.js
@@ -2,20 +2,42 @@
 //render component via function
 import React from 'react'
 
-export default function Box(props) {
+const DEFAULT_IMG =
+  "http://www.thewateringhole.co.uk/wp-content/uploads/2012/12/play.png";
 
-//     //in Function render, you can use "let" or "const"
-    const callme=(status)=>{
-        console.log(status)
-        alert("You have " + status +"!")
+
+export default function Box(props) {
+    
+    const winner = props.title === props.player;
+    //const winner = (props.title === props.player);
+    console.log(props.player)
+    let className
+    const hasPreviousGame =
+      props.player === "Computer" || props.player === "You";
+    if (hasPreviousGame) {
+      className = winner ? "winner" : "loser";
     }
+  
+    let prompt;
+    if (winner) {
+      prompt = "won";
+      className = winner ? "winner" : "loser";
+    } else if (props.player === "Tied") {
+      prompt = "tied";
+    } else if (!props.player) {
+      prompt = "";
+    } else {
+      prompt = "lost";
+    }
+  
+
     return (
-        <div className={`box ${props.color}`}  >
+        <div className={`box ${className}`}  >
         {/* start wrap */}
-        <p className={`para ${props.name}`}>My {props.name} is {props.color}</p>
-        <p><img alt="weapon" src={props.src}/></p> 
-        <p className={`para ${props.name}`}>and has {props.status}!</p>
-        <button onClick={()=>callme(props.status)}>See result!</button>
+        <h1>{props.title} {prompt}</h1>
+        <div>{props.title} has chosen {props.choice.name || "nothing"}</div>
+        <img alt ={props.choice.name} src={props.choice.url || DEFAULT_IMG}/>
+        {/* <div>{props.title} {prompt}</div> */}
         {/* end wrap */}
         </div>
     )
@@ -23,3 +45,16 @@ export default function Box(props) {
 //render component via function
 
 
+/*
+Box Computer: Win or Lose
+title: Computer
+
+Box You: Win or Lose
+title: You
+
+
+
+previousWinner: Computer or You
+
+
+*/
